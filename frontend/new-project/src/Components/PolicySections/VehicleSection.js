a22d00127b7bf10e86e546a6dc338aca48815577
McThuggets <dylan.reuben1509@gmail.com>
2023-08-01 13:54:40 +0200

    images and vehicle section changes

--- frontend/new-project/src/Components/PolicySections/VehicleSection.js
+++ frontend/new-project/src/Components/PolicySections/VehicleSection.js
@@ -1,15 +1,78 @@
 import React from 'react';
 import './vehicle.css';
-
+//images
+import  car from '../images/Car.svg'
+import VehicleSectionImage from '../images/VehicleSection.jpg'
 
 const VehicleDisplay = ({ vehicleData }) => {
 
   console.log(vehicleData);
 
   return (
+    <>
+<div className="main">
+                <div className="card">
+                  <div className="card-content">
+                    <img
+                      className="policy-img"
+                      src={VehicleSectionImage}
+                      alt="policy"
+                      style={{ height: "250px" }}
+                    />
+
+                    <span>
+                      <p className="top"></p>
+                      <h1 className="top-left">Policy Information</h1>
+                      <div className="text-container">
+                        <div>
+                          <p>Policy Holder:</p>
+                          <p></p>
+                        </div>
+                        <div>
+                          <p>Total Premium:</p>
+                          <p>R</p>
+                        </div>
+                        <div>
+                          <p>Policy start:</p>
+                          <p></p>
+                        </div>
+                        <div>
+                          <p>Intermediary fee:</p>
+                          <p></p>
+                        </div>
+                      </div>
+
+                      <hr />
+
+                      <button style={ {margin: "5px"}}>Documents</button>
+                    </span>
+
+                    </div>
+                </div>
+              </div>
+
+
+
     <div className="sectionMain">
       {vehicleData.risks.map((risk) => (
+
         <div key={risk.riskId} className="sectionCard">
+
+        {/* section for identifying info and image */}
+
+        <div className='vehicle-head'>
+          <div className='left-column'>
+            <h2> {risk.vehicleDetails.makeDescription + ' ' + risk.vehicleDetails.modelDescription} </h2>
+            <h2> {risk.vehicleDetails.yearManufactured}</h2>
+            <h2> {risk.vehicleDetails.registrationNumber}</h2>
+          </div>
+
+          <div className='right-column'>
+            <img src={car} width={'150px'}></img>
+          </div>
+          
+        </div>
+
           <h2>{risk.vehicleType} Details</h2>
           <p>
             <strong>Make:</strong> {risk.vehicleDetails.makeDescription}
@@ -69,6 +132,7 @@ const VehicleDisplay = ({ vehicleData }) => {
         </div>
       ))}
     </div>
+    </>
   );
 };
 

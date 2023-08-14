6ba3499cd737db74337406561f03753dcbbddbfe
ShikaneDecide <shikanedecide@gmail.com>
2023-08-14 08:34:33 +0200

    Login

--- frontend/new-project/src/Components/Login.js
+++ frontend/new-project/src/Components/Login.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaRegUser } from "react-icons/fa";
 import LoginBackground from "../Components/images/loginBackground.png";
 import "../Css/Login.css";
 
 const Login = () => {
+  const [showOTP, setShowOTP] = useState(false); // State to manage OTP input visibility
+
+  const handleSignIn = (e) => {
+    e.preventDefault();
+    // You can add your logic to validate the ID number here
+
+    // If ID number is valid, show OTP input
+    setShowOTP(true);
+  };
+
   return (
     <div className="body">
       <div className="center">
@@ -17,33 +27,39 @@ const Login = () => {
 
           <p className="Sign">Sign in to your account</p>
 
-          <div className="input-container">
-            <input
-              className="textbox"
-              type="text"
-              placeholder="ID Number"
-              disabled
-            />
-            <input
-              className="textbox"
-              type="email"
-              placeholder="Email"
-              disabled
-            />
-            <FaRegUser className="icon fas fa-user"></FaRegUser>
-          </div>
+          {showOTP && ( // Conditionally render OTP input
+            <div className="input-container">
+              <input
+                className="textbox"
+                type="text"
+                placeholder="Enter OTP"
+              />
+            </div>
+          )}
 
-          <div className="input-container">
-            <input
-              className="textbox"
-              type="text"
-              placeholder="Enter OTP"
-              disabled
-            />
-          </div>
-          <button type="submit" className="btnContinue">
-            SIGN IN
-          </button>
+          {!showOTP && ( // Conditionally render ID input
+            <div className="input-container">
+              <input
+                className="textbox"
+                type="text"
+                placeholder="ID Number"
+              />
+            </div>
+          )}
+
+          {showOTP ? ( // If OTP input is shown, display SIGN IN button
+            <button type="submit" className="btnContinue">
+              SIGN IN
+            </button>
+          ) : ( // Otherwise, display button to show OTP input
+            <button
+              type="button"
+              className="btnContinue"
+              onClick={handleSignIn}
+            >
+              SHOW OTP
+            </button>
+          )}
         </form>
       </div>
     </div>

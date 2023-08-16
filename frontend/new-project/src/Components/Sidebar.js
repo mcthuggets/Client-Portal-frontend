eb9a998e3a46c89966bf23246b9182e8b85c4e73
McThuggets <dylan.reuben1509@gmail.com>
2023-08-16 14:42:34 +0200

    minor sidebar css

--- frontend/new-project/src/Components/Sidebar.js
+++ frontend/new-project/src/Components/Sidebar.js
@@ -1,39 +1,60 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import '../Css/Sidebar.css';
-import {MdOutlineDashboard} from 'react-icons/md';
-import {MdOutlinePolicy} from 'react-icons/md';
-import {GrDocumentStore} from 'react-icons/gr';
-import {PiClipboardTextBold} from 'react-icons/pi';
-import {RiSecurePaymentLine} from 'react-icons/ri';
-import {CgProfile} from 'react-icons/cg';
+import React from "react";
+import { Link } from "react-router-dom";
+import "../Css/Sidebar.css";
+import { MdOutlineDashboard } from "react-icons/md";
+import { MdOutlinePolicy } from "react-icons/md";
+import { GrDocumentStore } from "react-icons/gr";
+import { PiClipboardTextBold } from "react-icons/pi";
+import { RiSecurePaymentLine } from "react-icons/ri";
+import { CgProfile } from "react-icons/cg";
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
-      
-    <ul >
-        <a href='/'><img src="https://www.fanews.co.za/images/cmsimages/big/brolink-logo_news_22667_17270.jpg"style={{width:"120px", height:"30px"}} alt="logo"/></a>
+      <div className="sticky">
+
+          <a href="/">
+            <img
+              src="https://www.fanews.co.za/images/cmsimages/big/brolink-logo_news_22667_17270.jpg"
+              style={{ width: "200px", height: "50px", margin: "10px"}}
+              alt="logo"
+            />
+          </a>
+
+        <ul>
           <li>
-            <Link to="/"><MdOutlineDashboard/>Dashboard</Link>
+            <Link to="/">
+              <MdOutlineDashboard  className="sidebar-icon"/>
+              Dashboard
+            </Link>
           </li>
-        
-            <li>
-              <Link to= "/policy/policies"><MdOutlinePolicy/>Policy</Link>
-            </li> 
-            <li>
-              <Link to="/policy/claims"><PiClipboardTextBold/>Claims</Link>
-            </li> 
-            <li>
-            <Link to="/policy/payments"><RiSecurePaymentLine/>Payment</Link>
-            </li>  
-            <li>
-              <Link to="/policy/clientdetails"><CgProfile/>My Details</Link>  
-          
+
+          <li>
+            <Link to="/policy/policies">
+              <MdOutlinePolicy />
+              Policy
+            </Link>
           </li>
-        
+          <li>
+            <Link to="/policy/claims">
+              <PiClipboardTextBold />
+              Claims
+            </Link>
+          </li>
+          <li>
+            <Link to="/policy/payments">
+              <RiSecurePaymentLine />
+              Payment
+            </Link>
+          </li>
+          <li>
+            <Link to="/policy/clientdetails">
+              <CgProfile />
+              My Details
+            </Link>
+          </li>
         </ul>
-   
+      </div>
     </div>
   );
 };

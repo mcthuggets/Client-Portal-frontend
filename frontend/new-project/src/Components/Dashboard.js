b45123174c6bbfd206a552cb544a22df018b4d72
ShikaneDecide <shikanedecide@gmail.com>
2023-09-18 09:07:05 +0200

    code Dashboard

--- frontend/new-project/src/Components/Dashboard.js
+++ frontend/new-project/src/Components/Dashboard.js
@@ -1,30 +1,3 @@
-// import Sidebar from "./Sidebar";
-// const Dashboard = ()=>{
-//     return (
-//         <>
-
-//         <div className="app-container">
-//         <Sidebar/>
-//         <div className='main-content'>
-//         <div className="dash-heading">
-//             <h1>Welcome, David</h1>
-//             <p>July 19, 2023</p>
-//         </div>
-//         <div className="dash-container">
-//             <h1>Billing and Payment</h1>
-//         <div className="dash-card">
-//             <p>123456789</p>
-//             <div className="separator"></div>
-
-//         </div>
-//         </div>
-//         </div>
-//         </div>
-//         </>
-//     )
-//     }
-// export default Dashboard;
-
 // Dashboard.js
 import React, { useEffect, useState } from "react";
 import axios from "axios";
